4e155e433aad45d97ba18b72533fbec41c870d02
Omer <omerdogan0@gmail.com>
2019-11-18 13:31:57 +0300

    Updates

---
+++ precache-manifest.5663cfb344df230ad4eeeb8f5eb61999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94042cf25fa910705fd42075b255a329",
+    "url": "/react-youtube/index.html"
+  },
+  {
+    "revision": "99cbe77bba1f9e771bee",
+    "url": "/react-youtube/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9400ddd64f0d81a60a44",
+    "url": "/react-youtube/static/css/main.cc1199cb.chunk.css"
+  },
+  {
+    "revision": "99cbe77bba1f9e771bee",
+    "url": "/react-youtube/static/js/2.ff95d32c.chunk.js"
+  },
+  {
+    "revision": "9400ddd64f0d81a60a44",
+    "url": "/react-youtube/static/js/main.42cf3172.chunk.js"
+  },
+  {
+    "revision": "c2d963ba099fda7aa06a",
+    "url": "/react-youtube/static/js/runtime-main.c27c959b.js"
+  }
+]);
